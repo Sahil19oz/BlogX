2ab5102a8aaf12acfad090609f0467970e761f39
Sahil19oz <30835204+Sahil19oz@users.noreply.github.com>
2020-05-03 19:20:30 +0530

    Added the html file

--- BlogX/BlogX/settings.py
+++ BlogX/BlogX/settings.py
@@ -56,7 +56,7 @@ ROOT_URLCONF = 'BlogX.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [TEMPLATE_DIR,],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
