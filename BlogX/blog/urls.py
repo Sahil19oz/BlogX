cba1900fdcfa93e9dd8ce026cddb23263b503b8b
Sahil19oz <30835204+Sahil19oz@users.noreply.github.com>
2020-05-04 18:57:17 +0530

    mondays commit

--- BlogX/blog/urls.py
+++ BlogX/blog/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .import views
+from . import views
 
 urlpatterns=[
+path("post/<int:pk>/",views.post_detail,name="post_detail"),
 path("",views.post_list,name="post_list"),
 ]
